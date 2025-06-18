dc2d465d80b371c323ad9a09768cd58c524fdf9b
zfeller <zfeller@ucsc.edu>
2025-06-18 10:56:24 -0700

    fix lint errors

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,15 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Typography from '@mui/material/Typography';
 
-
-// images 
-import image1 from './images/IMG_3025.jpeg'
-import image2 from './images/IMG_3027.jpeg'
-import image3 from './images/IMG_3028.jpeg'
-import image4 from './images/IMG_3029.jpeg'
-import image5 from './images/IMG_3030.jpeg'
-import image6 from './images/IMG_3031.jpeg'
 export default function Home() {
   return (
     <div style={{ width: "100vw", height:"100vh", margin: 0 }}>
